404278854726ae202f1086664d441cc827fc6df6
Peter Zhigalov <peter.zhigalov@gmail.com>
2024-12-14 22:59:16 +0700

    Updating LICENSE.txt to add info about version number

--- src/wintoastlibc.h
+++ src/wintoastlibc.h
@@ -1,5 +1,7 @@
 /**
- * Copyright (c) 2022-2023 Peter Zhigalov <peter.zhigalov@gmail.com>
+ * MIT License
+ *
+ * Copyright (C) 2022-2024 WinToastLibC v0.4 - Peter Zhigalov <peter.zhigalov@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
